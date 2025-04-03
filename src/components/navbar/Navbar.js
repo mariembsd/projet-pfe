c5bbb012fc4cd6b5adf6562f9d53aee186d04afc
mariem bsd <meriam.boussaadia@polytechnicien.tn>
2025-04-03 16:38:15 +0100

    about with s

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -33,7 +33,7 @@ const NavigationBar = () => {
               {/* Navigation Links */}
               {[
                 { name: "Homes", path: "/" },
-                { name: "About", path: "/about" },
+                { name: "Abouts", path: "/about" },
                 { name: "Services", path: "/services" },
                 { name: "Contact", path: "/contact" },
                 { name: "Dermatologist", path: "/dermatologist" },
